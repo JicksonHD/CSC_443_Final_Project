8e5e5a6e482d9eda3b06f399a2016540d6bdc363
JicksonHD <jacques.ghajar@lau.edu>
2022-12-15 21:33:38 +0200

    Writing controllers

--- Backend/socialmedia/routes/api.php
+++ Backend/socialmedia/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ImageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(["prefix"=>"v0.1"],function(){
+    Route::group(["prefix"=>"v0.1"],function(){
+        
+    });
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
