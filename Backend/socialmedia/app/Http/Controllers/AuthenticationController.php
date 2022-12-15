6af10adb69169361fb456b55e9437c133acff6f1
JicksonHD <jacques.ghajar@lau.edu>
2022-12-15 23:41:57 +0200

    Added login function in authenticationController

--- Backend/socialmedia/app/Http/Controllers/AuthenticationController.php
+++ Backend/socialmedia/app/Http/Controllers/AuthenticationController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Validator;
 
 class AuthenticationController extends Controller
 {
-    //
+    
+    function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        if($validate->fails()){
+            return response()->json([
+                "status" => "error",
+                "results" => "Some fields are empty"
+            ]);
+        }
+        $credentials = $request->only('email', 'password');
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'results' => 'Invalid Credentials',
+            ]);
+        }
+        $user = Auth::user();
+        return response()->json([
+                'status' => 'success',
+                'results' => 'Welcome',
+                'user' => $user,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ], 200);
+    }
 }
