5c02eb35bbd95a8f8e946091f8882f2064885e08
JicksonHD <jacques.ghajar@lau.edu>
2022-12-16 19:39:47 +0200

    Added jwt config

--- Backend/socialmedia/app/Models/User.php
+++ Backend/socialmedia/app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
+
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +44,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
