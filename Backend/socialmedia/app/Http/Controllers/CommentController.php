642c10dcb263e931bb052dec184a4d1aa63b8e47
JicksonHD <jacques.ghajar@lau.edu>
2022-12-16 02:27:14 +0200

    Added add comments function

--- Backend/socialmedia/app/Http/Controllers/CommentController.php
+++ Backend/socialmedia/app/Http/Controllers/CommentController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Comment;
+use Validator;
 
 class CommentController extends Controller
 {
-    //
+    function addComment(Request $request){
+        $validate = Validator::make($request->all(), [
+            'image_id' => 'required|integer',
+            'comment' => 'required|string'
+        ]);
+        if($validate->fails()){
+            return response()->json([
+                "status" => "error",
+                "results" => "Some fields are empty"
+            ]);
+        }
+        $user = Auth::user();
+        $user_id = $user->id;
+        //Check if post exists
+        $image = Post::find($request->image_id);
+        if(!$image){
+            return response()->json([
+                "status" => "error",
+                "results" => "Invalid Post"
+            ]);
+        }
+        // Adding new Comment
+        $comment = new Comment;
+        $comment->user_id = $user_id;
+        $comment->image_id = $request->image_id;
+        $comment->comment = $request->comment;
+        if($comment->save()){
+            return response()->json([
+                'status' => 'success',
+                'results' => 'Comment Added',
+                'comment' => $comment
+            ], 200);
+        }
+    }
 }
