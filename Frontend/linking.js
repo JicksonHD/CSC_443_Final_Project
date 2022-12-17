db25655ce3752c7b56b65fb1d5c586622d61e033
JicksonHD <jacques.ghajar@lau.edu>
2022-12-18 01:06:38 +0200

    Added scripts linking signup and linking file to each other

--- Frontend/linking.js
+++ Frontend/linking.js
@@ -77,3 +77,40 @@ pages.Console = (title, values, oneValue = true) => {
   };
   login_btn.addEventListener("click", login);
 };
+
+pages.load_signup = () =>{
+
+    const signup_btn = document.getElementById("signup");
+    const result = document.getElementById("response");
+
+    const responseHandler = () => {
+        result.innerHTML = '<div id = "response" class = "response_font"></div>';
+      };
+
+    const signup = async () => {
+
+        const signup_url = base_url + "signup.php";
+    
+        const signup_data = new URLSearchParams();
+        signup_data.append("first_name", document.getElementById("first_name").value);
+        signup_data.append("last_name", document.getElementById("last_name").value);
+        signup_data.append("email", document.getElementById("email").value);
+        signup_data.append("password", document.getElementById("password").value);
+    
+        const response = await pages.postAPI(signup_url,signup_data);
+        if (response.data.error) {
+          result.innerHTML =
+            '<div id = "response" class = "response_font">' +
+            response.data.error +
+            "</div>";
+          setTimeout(responseHandler, 2000);
+        } else {
+          result.innerHTML =
+            '<div id = "response" class = "response_font">' +
+            response.data.success +
+            "<br>Now Login!</div>";
+          setTimeout(responseHandler, 2000);
+        }
+      };
+      signup_btn.addEventListener("click", signup);
+};
