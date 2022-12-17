62bba83fb5d2e7eef51af2e38cbf1f9ebe5deb47
JicksonHD <jacques.ghajar@lau.edu>
2022-12-17 23:31:59 +0200

    Added postAPI function to linking file

--- Frontend/linking.js
+++ Frontend/linking.js
@@ -17,6 +17,19 @@ pages.Console = (title, values, oneValue = true) => {
     eval("pages.load_" + page + "();");
   };
 
+  pages.postAPI = async (api_url, api_data, api_token = null) => {
+    try {
+      return await axios.post(api_url, api_data, {
+        headers: {
+          Authorization: "token " + api_token,
+        },
+      });
+    } catch (error) {
+  pages.Console("Error from Post API", error);
+    }
+  };
+
+
   pages.getAPI = async (api_url) => {
     try {
       return await axios(api_url);
