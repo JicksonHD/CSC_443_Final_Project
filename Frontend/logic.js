a7b310d5a4c063cd9289fa3ca0642be3cf73cd45
JicksonHD <jacques.ghajar@lau.edu>
2022-12-17 21:24:03 +0200

    Added logic to function on click for heart

--- Frontend/logic.js
+++ Frontend/logic.js
@@ -1,4 +1,5 @@
 const moon = document.getElementById("moon");
+let heart = document.querySelector('img');
 
 moon.onclick = function(){
     document.body.classList.toggle("dark-theme");
@@ -7,4 +8,14 @@ moon.onclick = function(){
     }else{
         moon.src = "./moon.png";
     }
+}
+
+heart.onclick = function(){
+    console.log("I entered");
+    let mySrc = heart.getAttribute('src');
+    if(mySrc === "./empty_heart.png"){
+        heart.setAttribute('src',"./full_heart.png");
+    } else{
+        heart.setAttribute('src',"./empty_heart.png");
+    }  
 }
