068e72b6fbdb7eeb809dbd401fc4584aa393b0e9
JicksonHD <jacques.ghajar@lau.edu>
2022-12-17 21:55:52 +0200

    Added console (debugger method) in linking file

---
+++ Frontend/linking.js
@@ -0,0 +1,13 @@
+const pages = {};
+
+pages.Console = (title, values, oneValue = true) => {
+    console.log("---" + title + "---");
+    if (oneValue) {
+      console.log(values);
+    } else {
+      for (let i = 0; i < values.length; i++) {
+        console.log(values[i]);
+      }
+    }
+    console.log("--/" + title + "---");
+  };
