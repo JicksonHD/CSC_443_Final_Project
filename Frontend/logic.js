e603ad2b143fc7623608b48dea6ec82eea635482
JicksonHD <jacques.ghajar@lau.edu>
2022-12-19 01:58:16 +0200

    Added logic for comment icon

--- Frontend/logic.js
+++ Frontend/logic.js
@@ -1,5 +1,8 @@
 const moon = document.getElementById("moon");
-let heart = document.getElementById("heart");
+const heart = document.getElementById("heart");
+const comment_icons = document.getElementById("comment");
+const add_comment = document.getElementById("add_comment");
+const remove_comment_box = document.getElementById("x_icon");
 
 moon.onclick = function(){
     document.body.classList.toggle("dark-theme");
@@ -19,3 +22,17 @@ heart.onclick = function(){
         heart.src = "./empty_heart.png";
     }  
 }
+
+const commentHandler = (c) => {
+
+    add_comment.style.display = "flex";
+
+};
+
+const xicon = (x) => {
+
+    add_comment.style.display = "none";
+}
+
+comment_icons.forEach(b => b.addEventListener("click",commentHandler));
+remove_comment_box.addEventListener("click", xicon);
